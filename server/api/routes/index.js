9dceea717ef843c9a5bb66b6ad18c7692fc1d6b8
Dariya Baletskaya <baletskaya.dariya@gmail.com>
2019-08-08 14:21:12 +0300

    add auth routes, user service and auth service to provide auth logic, connect auth routes to the router

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,5 +1,7 @@
-import testRoutes from './test.routes';
+import testRoutes from "./test.routes";
+import authRoutes from "./auth.routes";
 // register all routes
-export default (app) => {
-    app.use('/api/test', testRoutes);
+export default app => {
+    app.use("/api/test", testRoutes);
+    app.use("/auth", authRoutes);
 };
