b521663a8dceec1ef1c4c719b36f391857cdfbee
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-09 18:40:08 +0300

    Add gameweek-history route

---
+++ server/api/routes/gameweek-history.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import * as gameweekHistoryService from "../services/gameweek-history.service";
+
+const router = Router();
+
+router
+    .get("/", (req, res, next) =>
+        gameweekHistoryService
+            .getAllHistory()
+            .then(value => res.json(value))
+            .catch(next)
+    )
+    .get("/:id", (req, res, next) =>
+        gameweekHistoryService
+            .getHistoryById(req.params.id)
+            .then(value => res.json(value))
+            .catch(next)
+    );
+
+export default router;
---
+++ server/data/repositories/gameweek-history.repository.js
@@ -0,0 +1,10 @@
+import { GameweekHistory } from '../models/index';
+import BaseRepository from './base.repository';
+
+class GameweekHistoryRepository extends BaseRepository {
+    getById(id) {
+        return this.model.findOne({ where: { id } });
+    }
+}
+
+export default new GameweekHistoryRepository(GameweekHistory);
