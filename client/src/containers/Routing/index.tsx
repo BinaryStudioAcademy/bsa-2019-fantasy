28e9ce72d299537331fd050eb17313f8289ce783
Olya Yakymovych <msolya@outlook.com>
2019-08-09 14:11:40 +0300

    fix pull request conflicts

--- client/src/containers/Routing/index.tsx
+++ client/src/containers/Routing/index.tsx
@@ -9,10 +9,14 @@ import Test from 'containers/Test';
 import NotFound from 'scenes/NotFound';
 import PrivateRoute from 'containers/PrivateRoute';
 import LoginPage from 'containers/Auth/Login/LoginPage';
-import RegistartionPage from 'containers/Auth/Registration/RegistartionPage';
+import RegistrationPage from 'containers/Auth/Registration/RegistrationPage';
+
 import Leagues from 'containers/Leagues';
 import CreateLeague from 'components/Leagues/CreateLeague';
 import JoinLeague from 'components/Leagues/JoinLeague';
+
+import Header from 'components/Header';
+import Sidebar from 'components/Sidebar';
 
 const Routing = () => {
     const dispatch = useDispatch();
@@ -27,35 +31,42 @@ const Routing = () => {
     }
 
     return (
-        <div className="fill">
-            {isAuthorized && <header>HEADER</header>}
-            <main className="fill">
-                <Switch>
-                    <Route exact path="/">
-                        <Test />
-                    </Route>
+        <div className='flex min-h-screen'>
+            <div className='flex-none h-screen'>{isAuthorized && <Sidebar />}</div>
+            <div className='flex-1 bg-background'>
+                {isAuthorized && <Header />}
+                <main className='mx-16 -mt-32'>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Test />
+                        </Route>
 
-                    <Route path="/login">
-                        <LoginPage />
-                    </Route>
+                        <Route path='/login'>
+                            <LoginPage />
+                        </Route>
 
-                    <Route path="/registration">
-                        <RegistartionPage />
-                    </Route>
+                        <Route path='/registration'>
+                            <RegistrationPage />
+                        </Route>
 
-                    <Route exact path="/leagues">
-                        <Leagues />
-                    </Route>
-                    <Route path='/leagues/create' component={CreateLeague} />
-                    <Route path='/leagues/join' component={JoinLeague} />
+                        <Route exact path="/leagues">
+                            <Leagues />
+                        </Route>
+                        <Route path="/leagues/create">
+                            <CreateLeague />
+                        </Route>
+                        <Route path="/leagues/join">
+                            <JoinLeague />
+                        </Route>                        
 
-                    <PrivateRoute exact path="/private">
-                        <Test />
-                    </PrivateRoute>
+                        <PrivateRoute exact path='/private'>
+                            <Test />
+                        </PrivateRoute>
 
-                    <Route path="*" component={NotFound} />
-                </Switch>
-            </main>
+                        <Route path='*' component={NotFound} />
+                    </Switch>
+                </main>
+            </div>
         </div>
     );
 };
