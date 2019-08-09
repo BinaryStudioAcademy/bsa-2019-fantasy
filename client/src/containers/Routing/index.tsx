76e07f2eaf16563b067ae6beb32e6e81b7fc3921
yugisu <slymahel@gmail.com>
2019-08-09 13:47:38 +0300

    Prettify project

--- client/src/containers/Routing/index.tsx
+++ client/src/containers/Routing/index.tsx
@@ -1,24 +1,22 @@
-import React, { useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { RootState } from "store/types";
-import { loadCurrentUser } from "containers/Profile/actions";
+import { RootState } from 'store/types';
+import { loadCurrentUser } from 'containers/Profile/actions';
 
-import Test from "containers/Test";
-import NotFound from "scenes/NotFound";
-import PrivateRoute from "containers/PrivateRoute";
-import LoginPage from "containers/Auth/Login/LoginPage";
-import RegistartionPage from "containers/Auth/Registration/RegistartionPage";
+import Test from 'containers/Test';
+import NotFound from 'scenes/NotFound';
+import PrivateRoute from 'containers/PrivateRoute';
+import LoginPage from 'containers/Auth/Login/LoginPage';
+import RegistrationPage from 'containers/Auth/Registration/RegistrationPage';
 
-import Header from "components/Header";
-import Sidebar from "components/Sidebar";
+import Header from 'components/Header';
+import Sidebar from 'components/Sidebar';
 
 const Routing = () => {
     const dispatch = useDispatch();
-    const { isLoading, isAuthorized } = useSelector(
-        (state: RootState) => state.profile
-    );
+    const { isLoading, isAuthorized } = useSelector((state: RootState) => state.profile);
 
     useEffect(() => {
         dispatch(loadCurrentUser());
@@ -29,31 +27,29 @@ const Routing = () => {
     }
 
     return (
-        <div className="flex min-h-screen">
-            <div className="flex-none h-screen">
-                {/*isAuthorized*/ true && <Sidebar />}
-            </div>
-            <div className="flex-1 bg-background">
-                {/*isAuthorized*/ true && <Header />}
-                <main className="mx-16 -mt-32">
+        <div className='flex min-h-screen'>
+            <div className='flex-none h-screen'>{isAuthorized && <Sidebar />}</div>
+            <div className='flex-1 bg-background'>
+                {isAuthorized && <Header />}
+                <main className='mx-16 -mt-32'>
                     <Switch>
-                        <Route exact path="/">
+                        <Route exact path='/'>
                             <Test />
                         </Route>
 
-                        <Route path="/login">
+                        <Route path='/login'>
                             <LoginPage />
                         </Route>
 
-                        <Route path="/registration">
-                            <RegistartionPage />
+                        <Route path='/registration'>
+                            <RegistrationPage />
                         </Route>
 
-                        <PrivateRoute exact path="/private">
+                        <PrivateRoute exact path='/private'>
                             <Test />
                         </PrivateRoute>
 
-                        <Route path="*" component={NotFound} />
+                        <Route path='*' component={NotFound} />
                     </Switch>
                 </main>
             </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,20 +2,20 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: "#1EE3CF",
-                primary2: "rgba(30, 227, 207, 0.55)",
-                secondary: "#122737",
-                secondary2: "rgba(18, 39, 55, 0.7)",
-                background: "#F2F2F2"
+                primary: '#1EE3CF',
+                primary2: 'rgba(30, 227, 207, 0.55)',
+                secondary: '#122737',
+                secondary2: 'rgba(18, 39, 55, 0.7)',
+                background: '#F2F2F2',
             },
             boxShadow: {
-                default: "0px 0px 19px rgba(0, 0, 0, 0.08)"
+                default: '0px 0px 19px rgba(0, 0, 0, 0.08)',
             },
             fontFamily: {
-                sans: ["Gilroy", "sans-serif"]
-            }
-        }
+                sans: ['Gilroy', 'sans-serif'],
+            },
+        },
     },
     variants: {},
-    plugins: []
+    plugins: [],
 };
