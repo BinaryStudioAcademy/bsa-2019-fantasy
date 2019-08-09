1923702a4b189c84751c034b6d692793c421d7a4
lsheva <lsheva7@gmail.com>
2019-08-10 01:21:37 +0300

    added empty routes n components

--- client/src/containers/Routing/index.tsx
+++ client/src/containers/Routing/index.tsx
@@ -8,8 +8,12 @@ import { loadCurrentUser } from 'containers/Profile/actions';
 import Test from 'containers/Test';
 import NotFound from 'scenes/NotFound';
 import PrivateRoute from 'containers/PrivateRoute';
+
 import LoginPage from 'containers/Auth/Login/LoginPage';
 import RegistrationPage from 'containers/Auth/Registration/RegistrationPage';
+
+import MyTeam from 'containers/MyTeam';
+import Transfers from 'containers/Transfers';
 
 import Leagues from 'containers/Leagues';
 import CreateLeague from 'components/Leagues/CreateLeague';
@@ -40,35 +44,20 @@ const Routing = () => {
         {isAuthorized && <Header />}
         <main className='mx-16 -mt-32'>
           <Switch>
-            <Route exact path='/'>
-              <Test />
-            </Route>
+            <Route path='/login' component={LoginPage} />
+            <Route path='/registration' component={RegistrationPage} />
 
-            <Route path='/login'>
-              <LoginPage />
-            </Route>
+            <Route path='/' exact component={Test} />
 
-            <Route path='/registration'>
-              <RegistrationPage />
-            </Route>
+            <Route path='/my-team' component={MyTeam} />
+            <Route path='/players' exact component={Players} />
+            <Route path='/transfers' exact component={Transfers} />
 
-            <Route exact path='/leagues'>
-              <Leagues />
-            </Route>
-            <Route path='/leagues/create'>
-              <CreateLeague />
-            </Route>
-            <Route path='/leagues/join'>
-              <JoinLeague />
-            </Route>
+            <Route path='/leagues' exact component={Leagues} />
+            <Route path='/leagues/create' component={CreateLeague} />
+            <Route path='/leagues/join' component={JoinLeague} />
 
-            <Route exact path='/players'>
-              <Players />
-            </Route>
-
-            <PrivateRoute exact path='/private'>
-              <Test />
-            </PrivateRoute>
+            <PrivateRoute exact path='/private' component={Test} />
 
             <Route path='*' component={NotFound} />
           </Switch>
