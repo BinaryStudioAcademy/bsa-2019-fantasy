be8f8307b18839f44090efe4a35fce764fa321f2
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-10 10:08:20 +0300

    Fix style and add types

--- client/src/components/Fixtures/Fixtures.tsx
+++ client/src/components/Fixtures/Fixtures.tsx
@@ -3,19 +3,56 @@ import moment from 'moment';
 
 import FixturesItem from './FixturesItem';
 
-const data = [
-    { date: '2019-08-09 15:30:11', team1: 'Liverpool', team2: 'LVP' },
-    { date: '2019-08-09  19:30:11', team1: 'Juventus', team2: 'NE-Juventus' },
-    { date: '2019-08-11 12:30:11', team1: 'Juventus', team2: 'Juventus' },
-    { date: '2019-08-11 12:30:11', team1: 'Dinamo', team2: 'NAVI' },
-    { date: '2019-09-09 19:30:11', team1: 'Barselona', team2: 'Arsenal' },
+type Props = {
+    gameweek?: string;
+    matches?: [
+        {
+            date: string;
+            team1: {
+                name: string;
+                avatar: string;
+            };
+            team2: {
+                name: string;
+                avatar: string;
+            };
+        },
+    ];
+};
+
+const fakeMatches = [
+    {
+        date: '2019-08-09 15:30',
+        team1: { name: 'Liverpool', avatar: '' },
+        team2: { name: 'LVP', avatar: '' },
+    },
+    {
+        date: '2019-08-09  19:30',
+        team1: { name: 'Juventus', avatar: '' },
+        team2: { name: 'NE-Juventus', avatar: '' },
+    },
+    {
+        date: '2019-08-11 12:30',
+        team1: { name: 'Juventus', avatar: '' },
+        team2: { name: 'Juventus', avatar: '' },
+    },
+    {
+        date: '2019-08-11 12:30',
+        team1: { name: 'Dinamo', avatar: '' },
+        team2: { name: 'Navi', avatar: '' },
+    },
+    {
+        date: '2019-09-09 19:30',
+        team1: { name: 'Barselona', avatar: '' },
+        team2: { name: 'Arsenal', avatar: '' },
+    },
 ];
 
-const Fixtures = ({ matches, gameweek }: any) => {
+const Fixtures = ({ matches, gameweek }: Props) => {
     const renderMessages = () => {
         let currentDate = '';
 
-        return data.flatMap((match: any) => {
+        return fakeMatches.flatMap((match: any) => {
             const res = [<FixturesItem data={match} key={`fixtures-${match.id}`} />];
 
             const messageDate = moment(match.date).format('dddd D MMMM YYYY');
@@ -36,11 +73,12 @@ const Fixtures = ({ matches, gameweek }: any) => {
     };
 
     return (
-        <div className='bg-white  py-6'>
+        <div className='bg-white py-3'>
             <div className='fixtures-list flex flex-col items-stretch text-center max-w-2xl'>
-                <h2 className='text-4xl'>Fixtures</h2>
-                <p className='my-3'>
-                    Gameweek {gameweek} - {moment(data[0].date).format('ddd D MMMM YYYY')}
+                <h2 className='text-5xl'>Fixtures</h2>
+                <p className='mb-3'>
+                    Gameweek {gameweek} -{' '}
+                    {moment(fakeMatches[0].date).format('ddd D MMMM YYYY')}
                 </p>
                 {renderMessages()}
             </div>
--- client/src/components/Fixtures/FixturesItem.tsx
+++ client/src/components/Fixtures/FixturesItem.tsx
@@ -3,27 +3,33 @@ import moment from 'moment';
 
 import './styles.scss';
 
-const FixturesItem = ({ data }: any) => {
+type Props = {
+    data: {
+        date: string;
+        team1: {
+            name: string;
+            avatar: string;
+        };
+        team2: {
+            name: string;
+            avatar: string;
+        };
+    };
+};
+
+const FixturesItem = ({ data }: Props) => {
     return (
         <li className='flex items-center p-3'>
             <div className='first-team team justify-end'>
-                <img
-                    className='logo order-1'
-                    src='https://i.pinimg.com/originals/59/d6/bb/59d6bb03b62928a02991ee1c706aa318.png'
-                    alt='logo'
-                />
-                <h5 className='font-bold'>{data.team1}</h5>
+                <img className='logo order-1' src={data.team1.avatar} alt='logo' />
+                <h5 className='font-bold'>{data.team1.name}</h5>
             </div>
             <div className='time p-3 py-2 rounded mx-2 play-time'>
                 <p>{moment(data.date).format('HH:mm')}</p>
             </div>
             <div className='second-team team'>
-                <img
-                    className='logo'
-                    src='http://allday2.com/uploads/posts/2017-09/1504853502_allday_1.jpg'
-                    alt='logo'
-                />
-                <h5 className='font-bold'>{data.team2}</h5>
+                <img className='logo' src={data.team2.avatar} alt='logo' />
+                <h5 className='font-bold'>{data.team2.name}</h5>
             </div>
         </li>
     );
