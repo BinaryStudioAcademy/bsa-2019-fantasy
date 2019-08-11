c6ec74fdb3c57a0e11229935f8ed097a2a41c085
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-11 19:18:54 +0300

    Add next and prev btn

--- client/src/components/Fixtures/Fixtures.tsx
+++ client/src/components/Fixtures/Fixtures.tsx
@@ -4,86 +4,72 @@ import moment from 'moment';
 import FixturesItem from './FixturesItem';
 
 type Props = {
-    gameweek?: string;
-    matches?: [
-        {
-            date: string;
-            team1: {
-                name: string;
-                avatar: string;
-            };
-            team2: {
-                name: string;
-                avatar: string;
-            };
-        },
-    ];
+  gameweek?: string;
+  matches?: [
+    {
+      date: string;
+      team1: {
+        name: string;
+        avatar: string;
+      };
+      team2: {
+        name: string;
+        avatar: string;
+      };
+    },
+  ];
 };
 
 const fakeMatches = [
-    {
-        date: '2019-08-09 15:30',
-        team1: { name: 'Liverpool', avatar: '' },
-        team2: { name: 'LVP', avatar: '' },
-    },
-    {
-        date: '2019-08-09  19:30',
-        team1: { name: 'Juventus', avatar: '' },
-        team2: { name: 'NE-Juventus', avatar: '' },
-    },
-    {
-        date: '2019-08-11 12:30',
-        team1: { name: 'Juventus', avatar: '' },
-        team2: { name: 'Juventus', avatar: '' },
-    },
-    {
-        date: '2019-08-11 12:30',
-        team1: { name: 'Dinamo', avatar: '' },
-        team2: { name: 'Navi', avatar: '' },
-    },
-    {
-        date: '2019-09-09 19:30',
-        team1: { name: 'Barselona', avatar: '' },
-        team2: { name: 'Arsenal', avatar: '' },
-    },
+  {
+    date: '2019-08-09 15:30',
+    team1: { name: 'Liverpool', avatar: '' },
+    team2: { name: 'LVP', avatar: '' },
+  },
+  {
+    date: '2019-08-09  19:30',
+    team1: { name: 'Juventus', avatar: '' },
+    team2: { name: 'NE-Juventus', avatar: '' },
+  },
+  {
+    date: '2019-08-11 12:30',
+    team1: { name: 'Juventus', avatar: '' },
+    team2: { name: 'Juventus', avatar: '' },
+  },
+  {
+    date: '2019-08-11 12:30',
+    team1: { name: 'Dinamo', avatar: '' },
+    team2: { name: 'Navi', avatar: '' },
+  },
+  {
+    date: '2019-09-09 19:30',
+    team1: { name: 'Barselona', avatar: '' },
+    team2: { name: 'Arsenal', avatar: '' },
+  },
 ];
 
 const Fixtures = ({ matches, gameweek }: Props) => {
-    const renderMessages = () => {
-        let currentDate = '';
+  const renderMessages = () => {
+    let currentDate = '';
 
-        return fakeMatches.flatMap((match: any) => {
-            const res = [<FixturesItem data={match} key={`fixtures-${match.id}`} />];
+    return fakeMatches.flatMap((match: any) => {
+      const res = [<FixturesItem data={match} key={`fixtures-${match.id}`} />];
 
-            const messageDate = moment(match.date).format('dddd D MMMM YYYY');
-            if (currentDate !== messageDate) {
-                res.unshift(
-                    <div
-                        className='fixtures-list__daystamp block'
-                        key={`daystamp-${messageDate}`}
-                    >
-                        <span>{messageDate}</span>
-                    </div>,
-                );
-                currentDate = messageDate;
-            }
+      const messageDate = moment(match.date).format('dddd D MMMM YYYY');
+      if (currentDate !== messageDate) {
+        res.unshift(
+          <div className='fixtures-list__daystamp block' key={`daystamp-${messageDate}`}>
+            <span>{messageDate}</span>
+          </div>,
+        );
+        currentDate = messageDate;
+      }
 
-            return res;
-        });
-    };
+      return res;
+    });
+  };
 
-    return (
-        <div className='bg-white py-3'>
-            <div className='fixtures-list flex flex-col items-stretch text-center max-w-2xl'>
-                <h2 className='text-5xl'>Fixtures</h2>
-                <p className='mb-3'>
-                    Gameweek {gameweek} -{' '}
-                    {moment(fakeMatches[0].date).format('ddd D MMMM YYYY')}
-                </p>
-                {renderMessages()}
-            </div>
-        </div>
-    );
+  return <React.Fragment>{renderMessages()}</React.Fragment>;
 };
 
 export default Fixtures;
--- client/src/containers/FixturesContainer/index.tsx
+++ client/src/containers/FixturesContainer/index.tsx
@@ -1,37 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 
-import { testAction } from './actions';
+import { loadGameweeksAction, loadGamesAction } from './actions';
 import { RootState } from 'store/types';
 
 import Fixtures from 'components/Fixtures/Fixtures';
+import Spinner from 'components/Spinner';
 
-//import styles from "./styles.module.scss";
+import './styles.scss';
 
 type Props = {
-  testRes: string;
-  testAction: typeof testAction;
+  gameweeks: string;
+  loadGameweeksAction: typeof loadGameweeksAction;
 };
 
-const FixturesContainer = () => {
-  const [gameweekId, setGameweekId] = useState(1);
+const FixturesContainer = ({
+  loadGameweeksAction,
+  loadGamesAction,
+  gameweeks,
+  games,
+}: any) => {
+  const [gameweeksList, setGameweeksList]: any = useState([]);
+  const [currentGameweek, setCurrentGameweek]: any = useState(0);
+  const [gamesList, setGamesList]: any = useState([]);
 
+  useEffect(() => {
+    loadGameweeksAction();
+  }, []);
+
+  useEffect(() => {
+    if (gameweeks) {
+      setGameweeksList(gameweeks);
+      loadGamesAction(gameweeks[currentGameweek].id);
+    }
+  }, [gameweeks]);
+
+  useEffect(() => {
+    if (games) {
+      setGamesList(games);
+    }
+  }, [games]);
+
+  if (!gameweeksList) {
+    return <Spinner />;
+  }
+  console.log(currentGameweek);
   return (
-    <div>
-      <button className='btn'>Prev</button>
-      <button className='btn'>Next</button>
-      <Fixtures />
+    <div className='bg-white py-3'>
+      <div className='fixtures-list flex flex-col items-stretch text-center max-w-2xl'>
+        <h2 className='text-5xl'>Fixtures</h2>
+        <p className='mb-3'>
+          Gameweek - {/* {moment(fakeMatches[0].date).format('ddd D MMMM YYYY')} */}
+        </p>
+        <div className='text-center text-white text-2xl mb-4 flex justify-between'>
+          {currentGameweek >= 1 && (
+            <button
+              className='btn bg-green-600 px-20 py-1 rounded'
+              onClick={() => setCurrentGameweek(currentGameweek - 1)}
+            >
+              Prev
+            </button>
+          )}
+          {currentGameweek < gameweeksList.length - 1 && (
+            <button
+              className='btn btn-next bg-green-600 px-20 py-1 rounded'
+              onClick={() => setCurrentGameweek(currentGameweek + 1)}
+            >
+              Next
+            </button>
+          )}
+        </div>
+        <Fixtures />
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = (rootState: RootState) => ({
-  testRes: rootState.test.testRes,
+  gameweeks: rootState.fixtures.gameweeks,
+  games: rootState.fixtures.games,
 });
 
 const actions = {
-  testAction,
+  loadGameweeksAction,
+  loadGamesAction,
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators(actions, dispatch);
