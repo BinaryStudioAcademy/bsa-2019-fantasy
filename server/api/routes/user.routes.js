ad2c584ab8782d9930fcc06f584ce6c186e68c67
Olya Yakymovych <msolya@outlook.com>
2019-08-13 01:19:26 +0300

    delete old route and started new one

--- server/api/routes/user.routes.js
+++ server/api/routes/user.routes.js
@@ -1,13 +1,21 @@
 import { Router } from 'express';
 import * as userService from '../services/user.service';
+import * as gameweekHistoryService from '../services/gameweek-history.service';
 
 const router = Router();
 
-router.get('/:id', (req, res, next) =>
+router
+  .get('/:id', (req, res, next) =>
     userService
-        .getUserById(req.params.id)
-        .then(value => res.json(value))
-        .catch(next)
-);
+      .getUserById(req.params.id)
+      .then((value) => res.json(value))
+      .catch(next),
+  )
+  .get('/:userId/gameweek-history/:gameweekId', (req, res, next) => {
+    const { userId, gameweekId } = req.params;
+    gameweekHistoryService
+      .getCurrentHistoryById(userId, gameweekId)
+      .then((value) => console.log(value));
+  });
 
 export default router;
