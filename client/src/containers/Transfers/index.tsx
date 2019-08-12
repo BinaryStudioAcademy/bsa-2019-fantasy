70be33922e4de0419bf4f78a6d64928787afdc91
Olya Yakymovych <msolya@outlook.com>
2019-08-12 23:39:10 +0300

    add transfer page

--- client/src/containers/Transfers/index.tsx
+++ client/src/containers/Transfers/index.tsx
@@ -1,48 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
 
-import { testAction } from './actions';
-import { RootState } from 'store/types';
-
-//import styles from "./styles.module.scss";
-
-type Props = {
-  testRes: string;
-  testAction: typeof testAction;
-};
-
-class Transfers extends React.Component<Props> {
-  static defaultProps = {
-    testRes: 'not received yet',
-  };
-
-  componentDidMount() {
-    //this.props.testAction();
-  }
-
-  render() {
-    const { testRes } = this.props;
-    return (
-      <div className='h-64 bg-white shadow rounded-sm p-12'>
-        Transfers page <br />
-        The test result is: {testRes}
+const Transfers = () => {
+  return (
+    <div className='transfers-page'>
+      <div className='jumbotron paper mb-12 rounded flex items-end justify-between pt-6'>
+        <div className='jumbotron-content mt-16'>
+          <h2 className='title text-secondary mb-6'>
+            <div className='sub title mb-4 flex items-center'>Transfers Page</div>
+            Transfers
+          </h2>
+        </div>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = (rootState: RootState) => ({
-  testRes: rootState.test.testRes,
-});
-
-const actions = {
-  testAction,
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators(actions, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Transfers);
+export default Transfers;
