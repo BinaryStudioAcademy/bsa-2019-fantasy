9c9f023bac905bfbd1d9f83e287e7368c0d099a5
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-12 23:10:31 +0300

    Update backend

--- server/data/repositories/game.repository.js
+++ server/data/repositories/game.repository.js
@@ -1,14 +1,24 @@
-import { GameModel } from '../models/index';
-import BaseRepository from './base.repository';
+import sequelize from "../db/connection";
+import { GameModel, FootballClubModel } from "../models/index";
+import BaseRepository from "./base.repository";
 
 class GameRepository extends BaseRepository {
-    getById(id) {
-        return this.model.findOne({ where: { id } });
-    }
+  getById(id) {
+    return this.model.findOne({ where: { id } });
+  }
 
-    getByGameweekId(id) {
-        return this.model.findAll({ where: { gameweek_id: id } });
-    }
+  getByGameweekId(id) {
+    return this.model.findAll({
+      where: { gameweek_id: id },
+
+      include: [
+        {
+          model: FootballClubModel,
+          attributes: ["name"]
+        }
+      ]
+    });
+  }
 }
 
 export default new GameRepository(GameModel);
