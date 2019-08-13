cfb9e0d1c39df8e8990fc1dbc08ba19f894a1da3
lsheva <lsheva7@gmail.com>
2019-08-14 00:52:44 +0300

    enable pagination sort on react-table

--- client/src/containers/Players/index.tsx
+++ client/src/containers/Players/index.tsx
@@ -4,6 +4,7 @@ import { bindActionCreators, Dispatch } from 'redux';
 import { Link } from 'react-router-dom';
 import { Option } from 'react-dropdown';
 import ReactTable from 'react-table';
+import _ from 'lodash';
 
 import { RootState } from 'store/types';
 import { Player } from 'types/player.types';
@@ -12,6 +13,7 @@ import Dropdown from 'components/Dropdown';
 import PlayerHighlight from 'components/PlayerHighlight';
 import SearchBar from 'components/SearchBar';
 import PlayerDialog from 'components/PlayerDialog';
+import { getClubLogoUrl } from 'helpers/images';
 
 import './styles.scss';
 
@@ -20,108 +22,85 @@ type Props = {
   loading: boolean;
   error: string | null;
   fetchPlayers: typeof fetchPlayers;
+  clubs: any;
 };
 
 type State = {
-  filters: any;
+  //filters: any;
   activePlayerId?: string | undefined;
 };
+
+const CLUB_LOGOS_ROUTE = '/images/club-logos/';
 
 class PlayersPage extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      filters: {
-        sortBy: 'price',
-        sortDirection: 'DESC',
-      },
+      activePlayerId: undefined,
     };
   }
 
-  filtersData = [
-    {
-      name: 'Sort by',
-      options: ['player_price', 'first_name', 'second_name'],
-      onChange: (value: any) => this.setState({ filters: { sortBy: value } }),
-    },
-    {
-      name: 'Sort order',
-      options: ['ASC', 'DESC'],
-      onChange: (value: any) => this.setState({ filters: { sortDirection: value } }),
-    },
-  ];
-  allPlayers = [
-    {
-      id: 'id1',
-      shirt: '/images/uniforms/field-players/shirt_1-66.png',
-      name: 'Lionel Messi',
-    },
-    {
-      id: 'id2',
-      shirt: '/images/uniforms/field-players/shirt_3-66.png',
-      name: 'Mario Balotelli',
-    },
-    {
-      id: 'id3',
-      shirt: '/images/uniforms/field-players/shirt_4-66.png',
-      name: 'Zlatan Ibrahimovic',
-    },
-    {
-      id: 'id4',
-      shirt: '/images/uniforms/field-players/shirt_1-66.png',
-      name: 'Lionelo Messi',
-    },
-    {
-      id: 'id5',
-      shirt: '/images/uniforms/field-players/shirt_3-66.png',
-      name: 'Mariotto Balotelli',
-    },
-    {
-      id: 'id6',
-      shirt: '/images/uniforms/field-players/shirt_4-66.png',
-      name: 'Zlatanus Ibrahimovic',
-    },
-  ];
-
   readonly columns = [
     {
-      Header: 'img',
       accessor: 'club_id',
-      //Cell: (props: any) => <img className='w-5 mr-4' src={props.value} alt='Shirt' />, // Custom cell components!
+      width: 70,
+
+      Cell: (props: any) => {
+        return (
+          <img className='px-2' src={getClubLogoUrl(props.value, '80')} alt='Shirt' />
+        );
+      }, // Custom cell components!
     },
     {
-      Header: 'First Name',
+      Header: 'Name',
+      className: 'flex items-center',
       accessor: 'first_name',
       Cell: (props: any) => (
         <Link className='mr-4 font-semibold' to='#'>
-          {props.value}
+          {props.value} {props.row.second_name}
         </Link>
       ),
     },
     {
       Header: 'Last Name',
       accessor: 'second_name',
-      Cell: (props: any) => (
-        <Link className='mr-4 font-semibold' to='#'>
-          {props.value}
-        </Link>
-      ),
+      show: false,
     },
     {
       Header: 'Price',
       accessor: 'player_price',
+      className: 'flex items-center',
     },
     {
       Header: 'Score',
       accessor: 'player_score',
+      className: 'flex items-center',
     },
     {
       Header: 'Position',
       accessor: 'position',
+      className: 'flex items-center',
+    },
+    {
+      Header: 'Club',
+      accessor: 'club_id',
+      className: 'flex items-center',
+      Cell: (props: any) => {
+        const club = this.props.clubs.clubs.find(({ id }: any) => id == props.value);
+        if (club) {
+          return (
+            <Link className='mr-4 font-semibold' to='#'>
+              {club.name}
+            </Link>
+          );
+        }
+        return null;
+      },
     },
     {
       Header: 'Info',
       //accessor: 'name',
+      className: 'flex items-center',
       Cell: () => (
         <button className='w-4 h-4 justify-center leading-none flex ml-auto bg-background rounded-full text-xs font-semibold'>
           i
@@ -131,24 +110,25 @@ class PlayersPage extends React.Component<Props, State> {
   ];
 
   onFilterChange = (opt: Option) => console.log(opt);
-  onFetchData = (state: any, instance: any) => {};
+  onFetchData = ({ page, pageSize, sorted }: any) => {
+    console.log('data fetch');
+    const defaultSort = { order_field: 'player_price', order_direction: 'DESC' };
+    const sort = sorted[0]
+      ? { order_field: sorted[0].id, order_direction: sorted[0].desc ? 'DESC' : 'ASC' }
+      : defaultSort;
+    this.props.fetchPlayers({
+      offset: page * pageSize,
+      limit: pageSize,
+      ...sort,
+    });
+    console.log(this.state);
+  };
 
   showModal = (id: string) => this.setState({ activePlayerId: id });
-  onModalDismiss = () => this.setState({ activePlayerId: '' });
-
-  componentDidMount() {
-    const req = { order_field: 'player_price', order_direction: 'DESC', limit: '10' };
-    this.props.fetchPlayers(req);
-  }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-    console.log(this.props.players);
-  }
+  onModalDismiss = () => this.setState({ activePlayerId: undefined });
 
   renderTable() {
-    if (!this.props.players.length) return 'spinner';
-    console.log(this.props.players);
+    //if (!this.props.players.length) return 'spinner';
     const playerTableData = this.props.players.map((player) => {
       if (!player) return {};
       const {
@@ -164,7 +144,8 @@ class PlayersPage extends React.Component<Props, State> {
     return (
       <ReactTable
         data={playerTableData}
-        // pages={this.state.pages} // should default to -1 (which means we don't know how many pages we have)
+        pageSize={10}
+        pages={10} // should default to -1 (which means we don't know how many pages we have)
         manual
         columns={this.columns}
         onFetchData={this.onFetchData}
@@ -181,15 +162,10 @@ class PlayersPage extends React.Component<Props, State> {
 
         <section className='allStats my-6'>
           <div className='filters text-sm flex my-6'>
-            {this.filtersData.map(({ name, options, onChange }, index) => (
-              <div className='filter' key={name}>
-                <Dropdown {...{ placeholder: name, options, onChange }} />
-              </div>
-            ))}
             <SearchBar />
           </div>
           <div className='bg-white shadow rounded my-4 p-6'>
-            <div className='column-header mb-4 font-semibold text-xs text-secondary2'>
+            <div className='column-header mb-4 font-semibold text-secondary2'>
               All Players
             </div>
             {this.renderTable()}
@@ -210,7 +186,7 @@ const mapStateToProps = (rootState: RootState) => ({
   players: rootState.players.players,
   loading: rootState.players.loading,
   error: rootState.players.error,
-  /*clubs: rootState.clubs,*/
+  clubs: rootState.clubs,
 });
 
 const actions = {
