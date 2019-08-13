99543b21d54ca9b63ecb442e253468415cdf0b44
lsheva <lsheva7@gmail.com>
2019-08-13 13:30:00 +0300

    refactor player repository, add max/min price

--- server/data/repositories/player.repository.js
+++ server/data/repositories/player.repository.js
@@ -1,28 +1,36 @@
+import { Op } from 'sequelize';
 import { PlayerStatModel } from '../models/index';
 import BaseRepository from './base.repository';
 
 class PlayerRepository extends BaseRepository {
-  async getPlayers(filter) {
-    const {
-      offset,
-      limit,
-      orderField,
-      orderDirection,
-      first_name,
-      second_name,
-      position,
-      club,
-    } = filter;
-
-    const where = {};
-
-    if (first_name) Object.assign(where, { first_name });
-    if (second_name) Object.assign(where, { second_name });
-    if (position) Object.assign(where, { position });
-    if (club) Object.assign(where, { club });
+  async getPlayers({
+    offset,
+    limit,
+    order_field,
+    order_direction,
+    first_name,
+    second_name,
+    position,
+    club_id,
+    max_price,
+    min_price,
+  }) {
+    // do not include if undefined
+    const where = {
+      ...(first_name && { first_name }),
+      ...(second_name && { second_name }),
+      ...(position && { position }),
+      ...(club_id && { club_id }),
+      ...((max_price || min_price) && {
+        player_price: {
+          ...(max_price && { [Op.lte]: max_price }),
+          ...(min_price && { [Op.gte]: min_price }),
+        },
+      }),
+    };
 
     const order = [];
-    if (orderField && orderDirection) order.push([orderField, orderDirection]);
+    if (order_field && order_direction) order.push([order_field, order_direction]);
 
     return this.model.findAll({ where, order, offset, limit });
   }
