bc6b003310cb7b11f34014f3bd58c706dc699c97
Olya Yakymovych <msolya@outlook.com>
2019-08-15 00:28:39 +0300

    fix indention and folder structure

---
+++ client/src/containers/Leagues/JoinLeague/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { FaStar } from 'react-icons/fa';
+
+import PrivateLeagues from './PrivateLeagues';
+import PublicLeagues from './PublicLeagues';
+
+import './styles.scss';
+
+const JoinLeague = () => (
+  <div className='join-league'>
+    <div className='container'>
+      <div className='jumbotron paper mb-12 rounded'>
+        <div className='jumbotron-content mt-12'>
+          <h2 className='title text-secondary'>
+            <div className='sub title mb-4 flex items-center'>
+              <FaStar />
+              Join a League
+            </div>
+            Join an existing league
+          </h2>
+        </div>
+      </div>
+      <div className='join-league-content paper flex flex-col md:flex-row'>
+        <PublicLeagues />
+        <PrivateLeagues />
+      </div>
+    </div>
+  </div>
+);
+
+export default JoinLeague;
--- client/src/containers/Leagues/index.tsx
+++ client/src/containers/Leagues/index.tsx
@@ -12,122 +12,129 @@ import './styles.scss';
 
 // mock data
 const mockData = {
-    leagues: ['Barcelona']
+  leagues: ['Barcelona'],
 };
 
+/* eslint-disable */
 const columns = [
-    {
-        Header: () => <span className="table-title uppercase font-bold">League</span>,
-        accessor: 'league',
+  {
+    Header: () => <span className='table-title uppercase font-bold'>League</span>,
+    accessor: 'league',
 
-        Cell: (props: { value: string }) => <span className="table-title-row">{props.value}</span>
-    },
-    {
-        Header: () => <span className="table-title uppercase font-bold">Current Rank</span>,
-        accessor: 'rank',
+    Cell: (props: { value: string }) => (
+      <span className='table-title-row'>{props.value}</span>
+    ),
+  },
+  {
+    Header: () => <span className='table-title uppercase font-bold'>Current Rank</span>,
+    accessor: 'rank',
 
-        Cell: (props: { value: { movement: string; current: number } }) => (
-            <div className="rank flex justify-center items-center">
-                <span className={`movement mr-1 ${props.value.movement === 'up' ? 'up' : 'down'}`}>
-                    {props.value.movement === 'up' ? <FaArrowUp /> : <FaArrowDown />}
-                </span>
-                {' '}
-                {props.value.current}
-            </div>
-        )
-    },
-    {
-        Header: () => <span className="table-title uppercase font-bold">Last Rank</span>,
-        accessor: 'rank',
+    Cell: (props: { value: { movement: string; current: number } }) => (
+      <div className='rank flex justify-center items-center'>
+        <span
+          className={`movement mr-1 ${props.value.movement === 'up' ? 'up' : 'down'}`}
+        >
+          {props.value.movement === 'up' ? <FaArrowUp /> : <FaArrowDown />}
+        </span>{' '}
+        {props.value.current}
+      </div>
+    ),
+  },
+  {
+    Header: () => <span className='table-title uppercase font-bold'>Last Rank</span>,
+    accessor: 'rank',
 
-        Cell: (props: { value: { movement: string; last: number } }) => (
-            <div className="rank flex justify-center items-center">
-                <span className={`movement mr-1 ${props.value.movement === 'up' ? 'up' : 'down'}`}>
-                    {props.value.movement === 'up' ? <FaArrowUp /> : <FaArrowDown />}
-                </span>
-                {' '}
-                {props.value.last}
-            </div>
-        )
-    }
+    Cell: (props: { value: { movement: string; last: number } }) => (
+      <div className='rank flex justify-center items-center'>
+        <span
+          className={`movement mr-1 ${props.value.movement === 'up' ? 'up' : 'down'}`}
+        >
+          {props.value.movement === 'up' ? <FaArrowUp /> : <FaArrowDown />}
+        </span>{' '}
+        {props.value.last}
+      </div>
+    ),
+  },
 ];
-
+/* eslint-enable */
 // mock data
 const table = [
-    {
-        league: 'Tanner Linsley',
-        movement: 'well',
-        rank: {
-            movement: 'up',
-            current: 123,
-            last: 23
-        }
+  {
+    league: 'Tanner Linsley',
+    movement: 'well',
+    rank: {
+      movement: 'up',
+      current: 123,
+      last: 23,
     },
-    {
-        league: 'Tanner Linsley',
-        movement: 'well',
-        rank: {
-            movement: 'down',
-            current: 123,
-            last: 23
-        }
-    }
+  },
+  {
+    league: 'Tanner Linsley',
+    movement: 'well',
+    rank: {
+      movement: 'down',
+      current: 123,
+      last: 23,
+    },
+  },
 ];
 
 const Leagues = () => {
-    const titles = [
-        {
-            title: 'Private classic leagues',
-            id: '0'
-        },
-        {
-            title: 'Public classic leagues',
-            id: '1'
-        },
-        {
-            title: 'Global leagues',
-            id: '2'
-        }
-    ];
+  const titles = [
+    {
+      title: 'Private classic leagues',
+      id: '0',
+    },
+    {
+      title: 'Public classic leagues',
+      id: '1',
+    },
+    {
+      title: 'Global leagues',
+      id: '2',
+    },
+  ];
 
-    return (
-        <div className="leagues">
-            <div className="container">
-                <div className="jumbotron paper mb-12 rounded flex items-end justify-between pt-6">
-                    <div className="jumbotron-content mt-12 mb-12">
-                        <h2 className="title mb-12 text-secondary">
-                            <div className="sub title mb-4 flex items-center">
-                                <FaStar />
-                                My Leagues
-                            </div>
-                            {map(mockData.leagues, (item, index) => (index === mockData.leagues.length - 1 ? item : `${item}, `))}
-                        </h2>
-                        <Link
-                            to="/leagues/join"
-                            className="bg-primary hover:bg-teal-400 text-secondary hover:text-white py-2 px-8 border-2 border-teal-300 rounded mr-6"
-                        >
-                            Join
-                        </Link>
-                        <Link
-                            to="/leagues/create"
-                            className="g-transparent hover:bg-teal-400 text-secondary hover:text-white py-2 px-6 border-2 border-gray-700 hover:border-transparent rounded"
-                        >
-                            New League
-                        </Link>
-                    </div>
-                    <div className="players flex">
-                        <img src={FirstPlayer} alt="player" />
-                        <img src={SecondPlayer} alt="player" />
-                    </div>
-                </div>
-                <div className="tables">
-                    {map(titles, item => (
-                        <LeagueTable columns={columns} data={table} title={item} key={item.id} />
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className='leagues'>
+      <div className='container'>
+        <div className='jumbotron paper mb-12 rounded flex items-end justify-between pt-6'>
+          <div className='jumbotron-content mt-12 mb-12'>
+            <h2 className='title mb-12 text-secondary'>
+              <div className='sub title mb-4 flex items-center'>
+                <FaStar />
+                My Leagues
+              </div>
+              {map(mockData.leagues, (item, index) =>
+                index === mockData.leagues.length - 1 ? item : `${item}, `,
+              )}
+            </h2>
+            <Link
+              to='/leagues/join'
+              className='bg-primary hover:bg-teal-400 text-secondary hover:text-white py-2 px-8 border-2 border-teal-300 rounded mr-6'
+            >
+              Join
+            </Link>
+            <Link
+              to='/leagues/create'
+              className='g-transparent hover:bg-teal-400 text-secondary hover:text-white py-2 px-6 border-2 border-gray-700 hover:border-transparent rounded'
+            >
+              New League
+            </Link>
+          </div>
+          <div className='players flex'>
+            <img src={FirstPlayer} alt='player' />
+            <img src={SecondPlayer} alt='player' />
+          </div>
         </div>
-    );
+        <div className='tables'>
+          {map(titles, (item) => (
+            <LeagueTable columns={columns} data={table} title={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Leagues;
