cc5750bd77dadcdfc39bffb04e1ac786f4eaa19f
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-14 09:41:30 +0300

    Add isLoading property

--- client/src/containers/FixturesContainer/actions.ts
+++ client/src/containers/FixturesContainer/actions.ts
@@ -2,6 +2,7 @@ import * as gameweekService from 'services/gameweekService';
 import {
   SET_GAMEWEEKS,
   SET_GAMES,
+  SET_IS_LOADING,
   setGameweekAction,
   setGamesAction,
   AsyncSetGameweekAction,
@@ -20,12 +21,21 @@ const setGames = (games: [FixturesItemType]): setGamesAction => ({
   payload: games,
 });
 
+const setIsLoading = (isLoading: boolean): setGamesAction => ({
+  type: SET_IS_LOADING,
+  payload: isLoading,
+});
+
+
 export const loadGameweeksAction = (): AsyncSetGameweekAction => async (dispatch) => {
+  
   const result = await gameweekService.getGameweeks();
   dispatch(setGameweeks(result));
 };
 
 export const loadGamesAction = (id: number): AsyncSetGamesAction => async (dispatch) => {
+  dispatch(setIsLoading(true));
   const result = await gameweekService.getGamesById(id);
   dispatch(setGames(result));
+  dispatch(setIsLoading(false));
 };
