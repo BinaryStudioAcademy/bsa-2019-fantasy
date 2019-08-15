a6005a94e84a9bfdc942dca9ddcc84854b5c12bf
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-15 21:48:49 +0300

    Add close btn

--- client/src/components/StatusPlayerModal/index.tsx
+++ client/src/components/StatusPlayerModal/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const StatusPlayerModal = ({ isCaptain, isViceCaptain }) => {
+import './styles.scss';
+
+const StatusPlayerModal = ({ isCaptain, isViceCaptain, onClose }: any) => {
   return (
     <div
       className='dimmer flex absolute inset-0 bg-modalDimmer'
@@ -11,8 +13,9 @@ const StatusPlayerModal = ({ isCaptain, isViceCaptain }) => {
         className='modal-status m-auto max-w-full max-h-full bg-white w-1/3 text-white'
         role='presentation'
       >
-        <div className='modal-header bg-green-700 p-4 font-bold text-xl'>
+        <div className='modal-header bg-green-700 p-4 font-bold text-xl flex justify-between'>
           <h3>Hello</h3>
+          <button onClick={() => onClose()}><span className='close'></span></button>
         </div>
         <div className='modal-body p-6 flex flex-col'>
           {!isCaptain && (
