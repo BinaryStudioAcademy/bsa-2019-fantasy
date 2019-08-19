4145971422f7d9a81820aee4de53c26bfb573ac5
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-19 20:06:39 +0300

    Clean code

--- client/src/helpers/handlers.ts
+++ client/src/helpers/handlers.ts
@@ -2,15 +2,13 @@ import { feedback } from 'react-feedbacker';
 import moment from 'moment';
 
 export default (socket) => {
-  socket.on('displayNotification', (fixtures) => {
-    if (fixtures && fixtures.length) {
-      fixtures.map((fix) => {
-        feedback.success(
-          `Your favorite club will play on ${moment(fix.start).format(
-            'dddd D MMMM YYYY HH:mm',
-          )}`,
-        );
-      });
+  socket.on('displayNotification', (fixture) => {
+    if (fixture) {
+      feedback.success(
+        `Your favorite club will play on ${moment(fixture.start).format(
+          'dddd D MMMM YYYY HH:mm',
+        )}`,
+      );
     } else {
       feedback.error(`We don't find games with your favorite club`);
     }
