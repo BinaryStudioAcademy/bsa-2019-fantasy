d088e432aa13b8d65019e41b19a1ec87b0a7725a
Dariya Baletskaya <baletskaya.dariya@gmail.com>
2019-08-20 18:02:42 +0300

    remove commented code

--- client/src/containers/GameweekHistory/index.tsx
+++ client/src/containers/GameweekHistory/index.tsx
@@ -10,21 +10,6 @@ import { getChartOptions } from 'helpers/gameweekChart';
 import styles from './styles.module.scss';
 import header from 'styles/header.module.scss';
 
-// const mockChartData = {
-//   labels: ['GW1', 'GW2', 'GW3', 'GW4', 'GW5', 'GW6', 'GW7'],
-//   datasets: [
-//     {
-//       label: 'points',
-//       fill: true,
-//       borderColor: '#1EE3CF',
-//       backgroundColor: 'rgba(30, 227, 207, 0.3)',
-//       pointHoverBackgroundColor: '#fff',
-//       pointHoverRadius: 7,
-//       data: [10, 59, 80, 81, 56, 55, 40],
-//     },
-//   ],
-// };
-
 const GameweekHistory = () => {
   const { t } = useTranslation();
 
