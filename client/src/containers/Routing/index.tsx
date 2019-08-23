1a1f0863918e25c1a05b469c90eaf4bdd9a8b407
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-23 20:49:50 +0300

    Fix triple notification

--- client/src/containers/Routing/index.tsx
+++ client/src/containers/Routing/index.tsx
@@ -74,14 +74,9 @@ const Routing = () => {
 
   useEffect(() => {
     dispatch(loadCurrentUser());
+    dispatch(fetchClubs());
+    dispatch(fetchGameweeks());
   }, [dispatch]);
-
-  useEffect(() => {
-    if (isAuthorized) {
-      dispatch(fetchClubs());
-      dispatch(fetchGameweeks());
-    }
-  }, [dispatch, isAuthorized]);
 
   useEffect(() => {
     if (isAuthorized && favorite_club) {
