f60bf61e12e3fc43fdc057f14442477d045e31f8
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2019-08-29 10:00:51 +0300

    update app-start

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/bsa-2019-fantasy/server/
-npm i
-npm i bcrypt --unsafe-perm
-npm rebuild
+yarn i
 npx sequelize-cli db:migrate
 npx sequelize-cli db:seed:all
 pm2 start --interpreter=./node_modules/.bin/babel-node server.js --name Fantasy
