473880bfe4c437eda0ab406efb51b9629687a5de
Olya Yakymovych <msolya@outlook.com>
2019-08-29 16:07:51 +0300

    fix bugs

--- server/helpers/crypto.helper.js
+++ server/helpers/crypto.helper.js
@@ -1,9 +1,9 @@
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcrypt";
 
 const saltRounds = 10;
 
 export default {
-    encrypt: data => bcryptjs.hash(data, saltRounds),
-    encryptSync: data => bcryptjs.hashSync(data, saltRounds),
-    compare: (data, encrypted) => bcryptjs.compare(data, encrypted)
+    encrypt: data => bcrypt.hash(data, saltRounds),
+    encryptSync: data => bcrypt.hashSync(data, saltRounds),
+    compare: (data, encrypted) => bcrypt.compare(data, encrypted)
 };
