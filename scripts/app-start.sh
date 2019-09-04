80d15506a66640669d458cd0fa8751c1f1fa021e
Oleksandr Kovalov <snake.dn@yandex.com>
2019-09-04 18:26:19 +0300

    Update app-start.sh

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -4,6 +4,3 @@ yarn
 npx sequelize-cli db:migrate
 npx sequelize-cli db:seed:all
 pm2 start --interpreter=./node_modules/.bin/babel-node server.js --name Fantasy
-cd /home/ubuntu/bsa-2019-fantasy/client/
-yarn
-xvfb-run --server-args=\"-screen 0 1920x1080x24\" ./node_modules/.bin/wdio wdio.conf.js
