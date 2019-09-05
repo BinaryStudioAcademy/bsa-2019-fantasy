42cd30127926d515f5c876964c2353d8df15a893
Alina Myrchenko <50015754+alinamrmr@users.noreply.github.com>
2019-09-05 23:03:28 +0300

    api-tests: created profile api test data

--- client/test/apiTests/data/profile.payload.js
+++ client/test/apiTests/data/profile.payload.js
@@ -1,3 +1,23 @@
+const updateUserFavoriteClubPayload = (clubId) => {
+    return{
+        clubId: clubId
+    };
+};
+
+const updateUserSendMailTimePayload = (sendmail_time) =>{
+    return{
+        userId: '',
+        sendmail_time = sendmail_time
+    };
+};
+
+const updateUserFixtureSubscriptionPayload = (game_id) => {
+    return{
+        userId: '',
+        game_id: game_id
+    };
+};
+
 const updateUserTeamDetailsPayload = (userData, teamMemberData) => {
   return {
     userId: '',
@@ -7,4 +27,9 @@ const updateUserTeamDetailsPayload = (userData, teamMemberData) => {
   };
 };
 
-module.exports = { updateUserTeamDetailsPayload };
+module.exports = { 
+    updateUserTeamDetailsPayload,
+    updateUserFavoriteClubPayload,
+    updateUserSendMailTimePayload,
+    updateUserFixtureSubscriptionPayload 
+};
--- client/test/apiTests/profile_apitest.js
+++ client/test/apiTests/profile_apitest.js
@@ -2,13 +2,35 @@ const request = require('supertest');
 const expect = require('chai').expect;
 const profilePayload = require('./data/profile.payload');
 const data = require('./data/profile.scenarios.json');
+const args = require('../specs/testData.json');
 
 const URL = 'http://ec2-18-224-246-75.us-east-2.compute.amazonaws.com:5001/api/';
 const PATH = `profile`;
 
 describe('Profile services test suite', () => {
   let payload;
+  let token;
 
+  beforeEach(async (token) => {
+    token = await fetch(`${URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          email: args.email,
+          password: args.password
+      })
+    })
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      return data.token;
+    });
+  }, token);
+
+  //PUT
   data.updateTeamDetailsScenarios.forEach((scenario) => {
     payload = Object.assign(
       {},
@@ -30,4 +52,93 @@ describe('Profile services test suite', () => {
       //   });
     });
   });
+
+  //POST
+  data.updateUserFavClubScenarios.forEach((scenario) => {
+    payload = Object.assign(
+      {},
+      profilePayload.updateUserFavoriteClubPayload(
+        scenario.clubId
+      )
+    );
+    it(scenario.testCaseName, () => {
+      // request(URL)
+      //   .put(`${PATH}/favorite-club`)
+      //   .send(payload)
+      //   .expect('Content-Type', /json/)
+      //   .expect(200)
+      //   .then((res) => {
+      //     // console.log('OK?', res.status);
+      //   });
+    });
+  });
+
+  //PUT
+  data.updateUserSendMailTimePayload.forEach((scenario) => {
+    payload = Object.assign(
+      {},
+      profilePayload.updateUserSendMailTimePayload(
+        scenario.sendmail_time
+      )
+    );
+
+    it(scenario.testCaseName, () => {
+      // request(URL)
+      //   .put(`${PATH}/${scenario.userId}`)
+      //   .send(payload)
+      //   .expect('Content-Type', /json/)
+      //   .expect(200)
+      //   .then((res) => {
+      //     // console.log('OK?', res.status);
+      //   });
+    })
+  });
+
+  //GET
+  data.getUserSubscribedFixturesScenarios.forEach((scenario) => {
+    it(scenario.testCaseName, () => {
+      // request(URL)
+      //   .put(`${PATH}/fixtures-sub/${scenario.userId}`)
+      //   .expect('Content-Type', /json/)
+      //   .expect(200)
+      //   .then((res) => {
+      //     // console.log('OK?', res.status);
+      //   });
+    });
+  });
+
+  //POST
+  data.updateUserFixtureSubscrScenarios.forEach((scenario) => {
+    payload = Object.assign(
+      {},
+      profilePayload.updateUserFixtureSubscriptionPayload(
+        scenario.game_id
+      )
+    );
+
+    it(scenario.testCaseName, () =>{
+      // request(URL)
+      //   .put(`${PATH}/fixtures-sub`)
+      //   .send(payload)
+      //   .expect('Content-Type', /json/)
+      //   .expect(200)
+      //   .then((res) => {
+      //     // console.log('OK?', res.status);
+      //   });
+    });
+  });
+
+  //DELETE
+  data.deleteUserFixtureSubscrScenarios.forEach((scenario) => {
+    it(scenario.testCaseName, () =>{
+      // request(URL)
+      //   .put(`${PATH}/fixtures-sub`)
+      //   .expect('Content-Type', /json/)
+      //   .expect(200)
+      //   .then((res) => {
+      //     // console.log('OK?', res.status);
+      //   });
+    });
+  });
+
 });
