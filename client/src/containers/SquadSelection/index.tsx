b016736e1da39b840434aa8a6170f9566e558ecf
lsheva <lsheva7@gmail.com>
2019-09-05 15:28:39 +0300

    fix position sticky TeamSelection component on 1080p

--- client/src/containers/SquadSelection/index.tsx
+++ client/src/containers/SquadSelection/index.tsx
@@ -59,17 +59,18 @@ const SquadSelection = withRouter(({ history }) => {
               onResetClick={() => handleResetSquad()}
               onAutoPickClick={() => handleAutoPick()}
             />
-
-            <TeamSelection
-              players={pitchPlayers}
-              setPlayers={setPitch}
-              showFixtures={false}
-              submit={{
-                label: t('Gameweek.saveTeam'),
-                canSubmit: amountOfPlayersPicked === 15,
-                onSubmit: () => setIsModalOpen(true),
-              }}
-            />
+            <div className='flex-1'>
+              <TeamSelection
+                players={pitchPlayers}
+                setPlayers={setPitch}
+                showFixtures={false}
+                submit={{
+                  label: t('Gameweek.saveTeam'),
+                  canSubmit: amountOfPlayersPicked === 15,
+                  onSubmit: () => setIsModalOpen(true),
+                }}
+              />
+            </div>
           </div>
           <PlayersSelection
             undisplayedPlayers={undisplayedPlayers as GameweekHistoryType[]}
