6f2db07008394bb0c020a867378e58e3177909f7
lsheva <lsheva7@gmail.com>
2019-09-09 17:02:38 +0300

    fix simulate socket connection

--- client/src/containers/Live/helpers/socket.ts
+++ client/src/containers/Live/helpers/socket.ts
@@ -1,6 +1,6 @@
 import socketIOClient from 'socket.io-client';
 
-const endpoint = `${process.env.REACT_APP_SOCKET_SERVER_PROTOCOL}://${process.env.REACT_APP_SOCKET_SERVER}:${process.env.REACT_APP_SOCKET_SERVER_PORT}/`;
+const endpoint = `${process.env.REACT_APP_SOCKET_SERVER_PROTOCOL}://${process.env.REACT_APP_SOCKET_SERVER}/`;
 const socket = socketIOClient(endpoint);
 
 export const simulate = ({ homeClubId, awayClubId }) => {
